c0ec7ff1ec87a708dd6567dd0ac1d8521871f960
Oscar Davalos <44180633+odavalos@users.noreply.github.com>
2021-07-07 15:17:35 -0700

    Update database_metadata_parser.py

--- database_metadata_parser.py
+++ database_metadata_parser.py
@@ -9,14 +9,14 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 
 # parser arguments
-parser.add_argument('--input','-1', type = argparse.FileType('r'), required = True, help = 'Input protein sequence file')
-parser.add_argument('--db_type','-db1', type = str, required = True, help = 'Database type name to give new column to label header id\'s')
+parser.add_argument('-in1', type = argparse.FileType('r'), required = True, help = 'Input protein sequence file')
+parser.add_argument('-db1', type = str, required = True, help = 'Database type name to give new column to label header id\'s')
 parser.add_argument('--merged_metadata', type = bool, default = False, help = 'whether extract metadata from both databases and to merge the files, default=False')
-parser.add_argument('--input_2','-2', type = argparse.FileType('r'), required = False, help = '2nd input sequence to merge the database metadata')
-parser.add_argument('--db_type2','-db2', type = str, required = False, help = 'Second database type name to give new column to label header id\'s')
-parser.add_argument('--output','-out1', type = str, required = True, help = 'Output file name')
-parser.add_argument('--output_2','-out2', type = str, required = False, help = 'Second output file name')
-parser.add_argument('--output_merged','-mergedout', type = str, required = False, help = 'Merged output file name')
+parser.add_argument('-in2', type = argparse.FileType('r'), required = False, help = '2nd input sequence to merge the database metadata')
+parser.add_argument('-db2', type = str, required = False, help = 'Second database type name to give new column to label header id\'s')
+parser.add_argument('-out1', type = str, required = True, help = 'Output file name')
+parser.add_argument('-out2', type = str, required = False, help = 'Second output file name')
+parser.add_argument('-mergedout', type = str, required = False, help = 'Merged output file name')
 
 args = parser.parse_args()
 
@@ -27,7 +27,7 @@ if args.merged_metadata == True:
 	# read the fasta file and isolate header lines and separate the strings
 	header_lines = []
 
-	with args.input  as file:
+	with args.in1  as file:
 		for line in file: 
 			if line.startswith('>'):
 				header_lines.append(line.strip())
@@ -67,13 +67,13 @@ if args.merged_metadata == True:
 
 	#print(f"First ten genome id's: {genome_id[:10]} \nFirst ten gene_numbers:{gene_number[:10]}")
 
-	db = [args.db_type] * len(genome_id)
+	db = [args.db1] * len(genome_id)
 
 	#create a dataframe that contains all information
 	database_metainfo = pd.DataFrame({'sequence_header':clean_header, 'genome_id':genome_id, 'gene_number':gene_number, 'database':db})
 
 	# save dataframe as csv file
-	database_metainfo.to_csv(f'{args.output}_metadata.csv',index = False)
+	database_metainfo.to_csv(f'{args.out1}_metadata.csv',index = False)
 	
 	print(f"Input_1 ==> First 10 names: {clean_header[:10]}")
 	print("#########################")
@@ -86,7 +86,7 @@ if args.merged_metadata == True:
 	########################## Input_2 ##########################
 	# read the fasta file and isolate header lines and separate the strings
 	header_lines = []
-	with args.input_2  as file:
+	with args.in2  as file:
 		for line in file: 
 			if line.startswith('>'):
 				header_lines.append(line.strip())
@@ -124,13 +124,13 @@ if args.merged_metadata == True:
 			genome_id.append(splitname[0])
 			gene_number.append(splitname[1])
 
-	db = [args.db_type2] * len(genome_id)
+	db = [args.db2] * len(genome_id)
 
 	#create a dataframe that contains all information
 	database_metainfo = pd.DataFrame({'sequence_header':clean_header, 'genome_id':genome_id, 'gene_number':gene_number, 'database':db})
 
 	# save dataframe as csv file
-	database_metainfo.to_csv(f'{args.output_2}_metadata.csv',index = False)
+	database_metainfo.to_csv(f'{args.out2}_metadata.csv',index = False)
 
 	print(f"Input_2 ==> First 10 names: {clean_header[:10]}")
 	print("#########################")
@@ -142,8 +142,8 @@ if args.merged_metadata == True:
 
 	########################## Merging Metadata ##########################
 
-	db_1 = pd.read_csv(f'{args.output}_metadata.csv')
-	db_2 = pd.read_csv(f'{args.output_2}_metadata.csv')
+	db_1 = pd.read_csv(f'{args.out}_metadata.csv')
+	db_2 = pd.read_csv(f'{args.out2}_metadata.csv')
 	merged_metadata = db_1.append(db_2, ignore_index=True) # does not attach second header as a row
 	merged_metadata.to_csv('mergedDB_metadata.csv',index = False)
 
